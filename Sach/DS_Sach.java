b45a7161960bdeb5e7d2aff2aaecbdde58ea6ec4
Jase Sous <jasesous@gmail.com>
2025-11-08 15:12:02 +0700

    Chỉnh nhiều thứ trong QLSP, DS_Sach, QLBH

--- Sach/DS_Sach.java
+++ Sach/DS_Sach.java
@@ -1,11 +1,7 @@
-import java.io.Serializable;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class DS_Sach implements Serializable {
-    // serialVersionUID
-    private static final long serialVersionUID = 1L;
-
+public class DS_Sach {
     // Thuoc tinh
     private int soLuongSach;
     private Sach[] dsSach;
@@ -23,6 +19,35 @@ public class DS_Sach implements Serializable {
 
     // Phương thức
     public void xem() {
-        System.out.println("");
+        System.out.println("\n===[Danh sách sản phẩm]===\n");
+        System.out.println("Số lượng: " + soLuongSach);
+        
+        if (soLuongSach > 0){
+            System.out.println("-".repeat(104));
+            System.out.printf(
+                    "\n| %-3s | %-7s | %-25s | %-11s | %-10s | %-6s | %-8s | %-9s |\n\n",
+                    "STT", "Mã sách", "Tên sách", "Mã thể loại", "Mã tác giả", "Mã NXB", "Số lượng", "Giá");
+            System.out.println("-".repeat(104));
+
+            for (int i = 0; i < soLuongSach; i++){
+                System.out.printf(
+                    "| %-3s | %-7s | %-25s | %-11s | %-10s | %-6s | %-8s | %-9s |\n",
+                    i+1,
+                    dsSach[i].getMasach(),
+                    dsSach[i].getTensach(),
+                    dsSach[i].getMatheloai(),
+                    dsSach[i].getMatg(),
+                    dsSach[i].getManxb(),
+                    dsSach[i].getSoluong(),
+                    dsSach[i].getGia());
+            }
+            System.out.println("-".repeat(104));
+        }
+    }
+
+    public void them(String masach, String tensach, String matheloai, String matg, String manxb, int soluong, int gia) {
+        dsSach = Arrays.copyOf(dsSach, dsSach.length + 1);
+        dsSach[soLuongSach] = new Sach(masach, tensach, matheloai, matg, manxb, soluong, gia);
+        soLuongSach++;
     }
 }
