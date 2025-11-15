3715814ffef965d326f24ac1f19cfa9f0494c751
Jase Sous <jasesous@gmail.com>
2025-11-16 00:33:45 +0700

    Gộp quản lý NCC

---
+++ Nhacungcap/DS_NCC.java
@@ -0,0 +1,276 @@
+package Nhacungcap;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class DS_NCC {
+    Scanner sc = new Scanner(System.in);
+
+    private int soluongNCC;
+    private Nhacungcap[] dsNCC;
+
+    // ===== Constructor =====
+    public DS_NCC() {
+    }
+    public DS_NCC(int soluongNCC,Nhacungcap []dsNCC)
+    {
+        this.soluongNCC=soluongNCC;
+        this.dsNCC=Arrays.copyOf(dsNCC, soluongNCC);
+    }
+    public DS_NCC(DS_NCC other)
+    {
+        this.dsNCC=Arrays.copyOf(other.dsNCC, other.soluongNCC);
+        this.soluongNCC=other.soluongNCC;
+    }
+        public  Nhacungcap [] getds(){return dsNCC;}
+
+    // ===== Nhập danh sách =====
+    public void loadFile(){
+        try(BufferedReader br = new BufferedReader(new FileReader("DATA/DS_NCC.dat"))){
+           
+        soluongNCC = Integer.parseInt(br.readLine().trim());
+        dsNCC = new Nhacungcap[soluongNCC]; // cấp phát mảng
+        for (int i = 0; i < soluongNCC; i++) {
+            dsNCC[i] = new Nhacungcap();
+            dsNCC[i].setmaNCC(br.readLine().trim());
+            dsNCC[i].settenNCC(br.readLine().trim());
+            dsNCC[i].setdiachi(br.readLine().trim());
+        }
+        br.close();
+        System.out.println("\n Nhap du lieu hoan tat");
+        }
+        catch (Exception e)
+        {
+            System.out.println("loi file");
+        }
+    }
+
+    // ===== Xuất danh sách =====
+    public void saveFile() {
+        try{
+            BufferedWriter bw=new BufferedWriter(new FileWriter("OUTPUT/DS_NCC.txt"));
+            BufferedWriter wb=new BufferedWriter(new FileWriter("DATA/DS_NCC.dat"));
+            bw.write(String.format("%-10s | %-20s |  %-20s%n", "Ma NCC", "Ten NCC", "Dia Chi"));
+            bw.write("--------------------------------------------------\n");
+
+            wb.write(String.valueOf(soluongNCC));
+            wb.newLine();
+            for (int i = 0; i < soluongNCC; i++) {
+                dsNCC[i].ghifile(bw,wb);
+            }
+            bw.close();
+            wb.close();
+            System.out.println("Da luu file va ghi file thanh cong");
+        }
+        catch ( Exception e){
+            System.out.println("lỗi đọc file");
+        }
+    }
+        public void xuatNCC() {
+        System.out.printf("%-10s | %-20s | %-20s%n", "MaNCC", "TenNCC", "Diachi");
+        for (Nhacungcap p : dsNCC) {
+            p.xuat();
+        }
+    }
+
+    // ===== Thêm nhà cung cấp =====
+    public void themNCC(Nhacungcap temp) {
+        dsNCC = Arrays.copyOf(dsNCC, soluongNCC + 1);
+        dsNCC[soluongNCC] = temp;
+        soluongNCC++;
+        System.out.println("Da them nha cung cap moi !");
+    }
+
+    // ===== Xóa nhà cung cấp =====
+    public void xoaNCC(String maNCCmuonxoa) {
+        int vitri = -1;
+        for (int i = 0; i < soluongNCC; i++) {
+            if (dsNCC[i].getmaNCC().equalsIgnoreCase(maNCCmuonxoa)) {
+                vitri = i;
+                break;
+            }
+        }
+
+        if (vitri == -1) {
+            System.out.println(" khong tim thay ma can xoa: " + maNCCmuonxoa);
+            return;
+        }
+
+        for (int i = vitri; i < soluongNCC - 1; i++) {
+            dsNCC[i] = dsNCC[i + 1];
+        }
+        soluongNCC--;
+        dsNCC = Arrays.copyOf(dsNCC, soluongNCC);
+        System.out.println("Da xoa nha cung cap ma: " + maNCCmuonxoa);
+    }
+//=============================sua thong tin nha cung cap=========================
+    public void suatenNCC(String mancc,String tencansua )
+    {
+        for(Nhacungcap i : dsNCC)
+        {
+            if(i.getmaNCC().equals(mancc))
+            {
+                i.settenNCC(tencansua);
+                break;
+            }
+            
+        }
+    }
+        public void suadiachi(String mancc,String diachisua )
+    {
+        for(Nhacungcap i : dsNCC)
+        {
+            if(i.getmaNCC().equals(mancc))
+            {
+                i.setdiachi(diachisua);
+                break;
+            }
+            
+        }
+    }
+    public void menusua(){
+        int choice;
+        
+        do {
+            System.out.println("\n===== MENU QUAN LY NHA CUNG CAP =====");
+            System.out.println("1. Sua ten NCC  ");
+            System.out.println("2. Sua dia chi  ");
+            System.out.println("0. Thoat chuc nang ");
+            System.out.println("=========================================");
+                choice = sc.nextInt();
+                sc.nextLine();
+            switch (choice) {
+                case 1:
+                    System.out.print("Nhap ma NCC can sua: ");
+                    String maNCC1 = sc.nextLine();
+                     System.out.print("Nhap ten moi : ");
+                    String tensua = sc.nextLine();
+                    suatenNCC(maNCC1,tensua);
+                    break;
+                case 2:
+                    System.out.print("Nhap ma NCC can sua: ");
+                    String maNCC2 = sc.nextLine();
+                    System.out.print("Nhap ten moi : ");
+                    String diachisua = sc.nextLine();
+                    suadiachi(maNCC2,diachisua);
+                    break;
+                case 0:
+                    break;
+                default:
+                    System.out.println("Lua chon khong hop le. Vui long chon lai.");
+            }
+        } while (choice != 0);
+    }
+    // ===== Tìm theo mã =====
+    public Nhacungcap timtheomaNCC(String maNCC) {
+        for (int i = 0; i < soluongNCC; i++) {
+            if (dsNCC[i].getmaNCC().equals(maNCC)) {
+                return dsNCC[i];
+            }
+        }
+        return null;
+    }
+
+    // ===== Tìm theo tên =====
+    public Nhacungcap[] timtheotenNCC(String ten) {
+        Nhacungcap[] kq = new Nhacungcap[0];
+        int count = 0;
+        for (int i = 0; i < soluongNCC; i++) {
+            if (dsNCC[i].gettenNCC().toLowerCase().contains(ten.toLowerCase())) {
+                kq = Arrays.copyOf(kq, count + 1);
+                kq[count] = dsNCC[i];
+                count++;
+            }
+        }
+        return kq;
+    }
+
+    // ===== Menu tìm kiếm =====
+    public void menutimNCC() {
+        int chon;
+        do {
+            System.out.println("\n===== MENU tim kiem nha cung cap =====");
+            System.out.println("1. tim theo ma");
+            System.out.println("2. Tìm theo ten");
+            System.out.println("0. Thoat");
+            System.out.print("Chon: ");
+            chon = sc.nextInt();
+            sc.nextLine();
+
+            switch (chon) {
+            case 1:
+                System.out.print("Nhap ma NCC can tim: ");
+                String ma = sc.nextLine();
+                Nhacungcap NCC = timtheomaNCC(ma);
+                if (NCC != null) {
+                    System.out.println("\nKQ:");
+                    System.out.printf("%-10s | %-20s | %-20s%n", "Ma NCC", "Ten NCC", "Đia chi");
+                    NCC.xuat();
+                } else {
+                    System.out.println(" Khong tim thay ma NCC: " + ma);
+                }
+                break;
+
+            case 2:
+                System.out.print("Nhap ten can tim: ");
+                String ten = sc.nextLine();
+                Nhacungcap[] ds = timtheotenNCC(ten);
+                if (ds.length == 0) {
+                    System.out.println("ko tim thay ten : " + ten);
+                } else {
+                    System.out.printf("%-10s %-20s %-20s%n", "Ma NCC", "Ten NCC", "Dia chi");
+                    for (Nhacungcap n : ds)
+                        n.xuat();
+                }
+                break;
+
+            case 0:
+                System.out.println("Thoat menu tim kiem.");
+                break;
+
+            default:
+                System.out.println("lua chon ko hop le!");
+                break;
+            }
+        } while (chon != 0);
+  
+    }
+    //------------------thong ke ------------------------
+    public void ThongkekhuvucNCC (){
+        int[] tk=new int[3];
+        for(Nhacungcap p: dsNCC)
+        {
+            if(p.getdiachi().contains("HCM"))
+            {
+                tk[0]++;
+            }
+            else if (p.getdiachi().contains("HN"))
+            {
+                tk[1]++;
+            }
+            else 
+            {
+                tk[2]++;
+            }
+        }
+        for (int i=0;i<tk.length;i++)
+        {
+            if(i==0)
+            {
+                System.out.printf("%-10s : %-10s%n","So NCC den tu HCM :", tk[i] );
+            }
+            else if(i==1)
+            {
+                System.out.printf("%-10s : %-10s%n","So NCC den tu HN :", tk[i] );
+            }
+            else
+            {
+                System.out.printf("%-10s : %-10s%n","So NCC den tu DN :", tk[i] );
+            }
+        }
+    }
+        
+}
--- QuanLy/QLBH.java
+++ QuanLy/QLBH.java
@@ -2,6 +2,7 @@ package QuanLy;
 import Sach.DS_Sach;
 import Khachhang.DS_Khachhang;
 import NXB.DS_NXB;
+import Nhacungcap.DS_NCC;
 import Nhanvien.DS_Nhanvien;
 import Nhaphang.DS_CTPNH;
 //import Hoadon.DS_Hoadon;
@@ -13,12 +14,12 @@ public class QLBH implements IQuanLy {
     protected static DS_Sach ds_Sach = new DS_Sach();
     protected static DS_Khachhang ds_Khachhang = new DS_Khachhang();
     protected static DS_Nhanvien ds_Nhanvien = new DS_Nhanvien();
-    //protected static DS_Hoadon ds_Hoadon = new DS_Hoadon();
     protected static DS_NXB ds_NXB = new DS_NXB();
     protected static DS_PhieuNhap ds_Phieunhap = new DS_PhieuNhap();
     protected static DS_CTPNH ds_CTPNH = new DS_CTPNH();
     protected static DS_HoaDon ds_Hoadon = new DS_HoaDon();
     protected static DS_ChiTietHoaDon ds_ChitietHoaDon = new DS_ChiTietHoaDon();
+    protected static DS_NCC ds_NCC = new DS_NCC();
 
     public void loadFile(){
         ds_Sach.loadFile();
@@ -29,6 +30,7 @@ public class QLBH implements IQuanLy {
         ds_NXB.loadFile();
         ds_Phieunhap.loadFile();
         ds_CTPNH.loadFile();
+        ds_NCC.loadFile();
     }
 
     public void menuChinh(){}
